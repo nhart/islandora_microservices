e760d1dd2e0452b7fda560c1855eb04f1f3c5b3a
Nelson Hart <nelson.hart@gmail.com>
2013-07-03 08:35:59 -0300

    first check for existing poicy on ingested object

--- plugins/coalliance/__init__.py
+++ plugins/coalliance/__init__.py
@@ -40,17 +40,18 @@ class coalliance(IslandoraListenerPlugin):
                 self.processMessage(dsid, obj, comime)
             #copy parent child policy to object being ingested
             try:
-                relsext = rels_ext(obj, rels_namespace('fedora', 'info:fedora/fedora-system:def/relations-external#'), 'fedora')
-                relationship = relsext.getRelationships('isMemberOfCollection')
-                if relationship:
-                    parent_pid = relationship[0][2]
-                    parent_obj = client.getObject(parent_pid)
-                    if parent_obj:
-                        if 'CHILD_SECURITY' in parent_obj and 'POLICY' not in 'obj':
-                            child_policy = parent_obj['CHILD_SECURITY']
-                            policy_xml = child_policy.getContent().read()
-                            obj.addDataStream('POLICY', policy_xml, controlGroup=u'M', label=u'Xacml Policy Stream', mimeType=u'text/xml', logMessage=u'Microservices added child policy to object')
-                            self.logger.info('Copy CHILD_SECURITY datastream from parent. Pid %s' % message['pid'])
+                if 'POLICY' not in 'obj':
+                    relsext = rels_ext(obj, rels_namespace('fedora', 'info:fedora/fedora-system:def/relations-external#'), 'fedora')
+                    relationship = relsext.getRelationships('isMemberOfCollection')
+                    if relationship:
+                        parent_pid = relationship[0][2]
+                        parent_obj = client.getObject(parent_pid)
+                        if parent_obj:
+                            if 'CHILD_SECURITY' in parent_obj:
+                                child_policy = parent_obj['CHILD_SECURITY']
+                                policy_xml = child_policy.getContent().read()
+                                obj.addDataStream('POLICY', policy_xml, controlGroup=u'M', label=u'Xacml Policy Stream', mimeType=u'text/xml', logMessage=u'Microservices added child policy to object')
+                                self.logger.info('Copy CHILD_SECURITY datastream from parent. Pid %s' % message['pid'])
             except:
                 pass
                 self.logger.error('Can not copy CHILD_SECURITY datastream from parent. Pid %s' % message['pid'])
